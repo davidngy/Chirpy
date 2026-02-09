a922344e46c6ef936b74af1d0a9d2e64a8ef4f24
david nguyen <davidngy2803@gmail.com>
2026-02-09 17:35:24 +0100

    adding readiness endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
-
+import { handlerReadiness } from "./handlerReadiness.js";
 const app = express();
 const PORT = 8080;
 
 app.use(express.static("."));
+app.use(express.static("./assets/logo.png"));
+app.get("/healthz", handlerReadiness);
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
