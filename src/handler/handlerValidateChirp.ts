1a5e44e2abe5ba5d948bc366489a19a01bb80b1f
david nguyen <davidngy2803@gmail.com>
2026-02-11 23:36:17 +0100

    peeping profone words

--- src/handler/handlerValidateChirp.ts
+++ src/handler/handlerValidateChirp.ts
@@ -1,6 +1,6 @@
-import { NextFunction, Response, Request } from "express";
+import { Response, Request } from "express";
 
-export async function validateChirp(req: Request, res: Response, next: NextFunction) {
+export async function handlerValidateChirp(req: Request, res: Response) {
     let body = "";
 
     req.on("data", (chunk) => {
@@ -8,10 +8,16 @@ export async function validateChirp(req: Request, res: Response, next: NextFunct
     })
 
     req.on("end", () => {
+        let parsedBody;
         try {
-            const parsedBody = JSON.parse(body);
+            parsedBody = JSON.parse(body);
         } catch(error) {
             res.status(400).send("Invalid JSON")
         }
+        const chirp = parsedBody.body;
+        if(chirp.length > 140) {
+            res.status(400).send({"error": "Chirp is too long"})
+        }
+        res.status(200).send({"valid": true})
     })
 }
--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { handlerMetrics } from "./handler/handlerMetrics.js";
 import { handlerMetricsReset } from "./handler/handlerMetricsReset.js";
 import { logResponses } from "./middleware/logResponses.js";
 import { middlewareMetricsInc } from "./middleware/metricsInc.js";
-
+import { handlerValidateChirp } from "./handler/handlerValidateChirp.js";
 const app = express();
 const PORT = 8080;
 app.use(logResponses);
@@ -13,6 +13,7 @@ app.use("/app", middlewareMetricsInc, express.static("./src/app"));
 app.get("/api/healthz", handlerReadiness);
 app.get("/admin/metrics", handlerMetrics);
 app.post("/admin/reset", handlerMetricsReset);
+app.post("/api/validate_chirp", handlerValidateChirp);
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
