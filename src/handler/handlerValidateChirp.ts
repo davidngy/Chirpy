168b415aa1611b5d0a2b5dd9a64dca2902f30ebd
david nguyen <davidngy2803@gmail.com>
2026-02-18 00:24:31 +0100

    adding storage

--- src/handler/handlerValidateChirp.ts
+++ src/handler/handlerValidateChirp.ts
@@ -1,20 +1,27 @@
 import { Response, Request } from "express";
-
+import { BadRequestError } from "../error/httpErrors.js";
 export async function handlerValidateChirp(req: Request, res: Response) {
-    const forbidden = ["kerfuffle", "sharbert", "fornax"];
-    const chirp = req.body;
-    if(chirp.length > 140) {
-        throw new Error("Chirp is too long");
+  type parameters = {
+    body: string;
+  };
+  const params: parameters = req.body;
+  const forbidden = ["kerfuffle", "sharbert", "fornax"];
+  const chirp = params.body;
+  console.log(req.body);
+  console.log(chirp);
+  if (chirp.length > 140) {
+    throw new BadRequestError("Chirp is too long. Max length is 140");
+  }
+  const chirpArray = chirp.split(" ");
+  const cleanedBodyArr = [];
+  for (const word of chirpArray) {
+    if (forbidden.includes(word.toLowerCase())) {
+      cleanedBodyArr.push("****");
+    } else {
+      cleanedBodyArr.push(word);
     }
-    const chirpArray = chirp.split(" ");
-    const cleanedBodyArr = [];
-    for(const word of chirpArray) {
-        if(forbidden.includes(word.toLowerCase())) {
-            cleanedBodyArr.push("****");
-        } else {
-            cleanedBodyArr.push(word);
-        }
-    }
-    const cleanedBody = cleanedBodyArr.join(" ");
-    res.status(200).send({"cleanedBody": cleanedBody})
+  }
+  const cleanedBody = cleanedBodyArr.join(" ");
+  res.status(200).send({ cleanedBody: cleanedBody });
 }
+
--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -1,14 +1,31 @@
 import { NextFunction, Response, Request } from "express";
-
+import {
+  NotFoundError,
+  BadRequestError,
+  ForbiddenError,
+  UnauthorizedError,
+} from "../error/httpErrors.js";
 export function errorHandler(
   err: Error,
   req: Request,
   res: Response,
   next: NextFunction,
 ) {
-    console.error("Something went wrong on our end");
-    res.status(500).json({
-        "error": "Something went wrong on our end"
-    });
+  let statusCode = 500;
+  if (err instanceof NotFoundError) {
+    statusCode = 404;
+    res.status(statusCode).send({ error: err.message });
+  } else if (err instanceof BadRequestError) {
+    statusCode = 400;
+    res.status(statusCode).send({ error: err.message });
+  } else if (err instanceof ForbiddenError) {
+    statusCode = 403;
+    res.status(statusCode).send({ error: err.message });
+  } else if (err instanceof UnauthorizedError) {
+    statusCode = 401;
+    res.status(statusCode).send({ error: err.message });
+  }
+  if (statusCode === 500) {
+    console.log(err.message);
+  }
 }
-
